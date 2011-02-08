1b7ca13b9cfbf9e6cdd116d1e78f0d361957c970
tsolbjor <thomas@solbjor.no>
2011-02-08 22:33:04 +0100

    VS solution

---
+++ src/tsol/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tsol
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmptyOr(this string s, string value)
+        {
+            return string.IsNullOrEmpty(s) || s == value;
+        }
+    }
+}
